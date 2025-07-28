1012febd72f0e0ca5cd3815a8107321f81fab14d
Jair Terrones <jair.terrones@evaseguros.es>
2025-07-28 16:56:58 -0500

    update desing overview

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -3,12 +3,21 @@ import Link from 'next/link';
 
 export function Navbar() {
   return (
-    <div className="grid items-center justify-between text-green-d font-medium md:grid-cols-2 gap-4 md:gap-20 md:px-20 md:text-lg">
-      <Image src="/assets/Recopa-logo-Color.png" alt='Logo Principal' width={200} height={25} className='mx-auto lg:mx-0'/>
-      <div className="flex gap-4 text-center justify-center">
-        <Link href="#overview">Nosotros</Link>
-        <Link href="#feactures">Proyectos</Link>
-        <Link href="#about">Contáctanos</Link>
+    <div className="grid items-center justify-between px-5 text-green-dark font-medium md:grid-cols-2 gap-4 md:gap-20 md:px-24 md:py-7 md:text-lg">
+      {/* <div className='flex items-end gap-2'>
+        <div>
+      <Image src="/assets/logo.png" alt='Logo Principal' width={69} height={52} className='mx-auto lg:mx-0 lg:w-[69px]'/>
+        </div>
+        <div className='uppercase'>
+          <h2 className='text-sm'>Inversiones</h2>
+          <h2 className='text-2xl'>Recopa</h2>
+        </div>
+      </div> */}
+      <Image src="/assets/logo3.png" alt='Logo Principal' width={250} height={250} className='mx-auto lg:mx-0 lg:w-[250px]'/>
+      <div className=" flex gap-20  text-center justify-center">
+        <Link className='hover:bg-green-100 rounded-xl px-4 py-2' href="#overview">Nosotros</Link>
+        <Link className='hover:bg-green-100 rounded-xl px-4 py-2' href="#feactures">Proyectos</Link>
+        <Link className='hover:bg-green-100 rounded-xl px-4 py-2' href="#about">Contáctanos</Link>
       </div>
 
     </div>
--- src/app/components/Overview/Overview.tsx
+++ src/app/components/Overview/Overview.tsx
@@ -5,15 +5,25 @@ import Link from "next/link";
 export function Overview() {
   return (
     <>
-      <div id="overview" className="p-5 md:px-24 md:py-20 bg-[#f4f6f5]">
-        <div className="items-center md:grid md:grid-cols-2 md:gap-10">
+      <div id="overview" className="px-5 py-10 md:px-24 md:py-10">
+        <div className=" md:grid md:grid-cols-2 md:gap-10">
           <div>
-            <h1 className="uppercase text-green-dark text-4xl font-bold md:leading-snug md:text-6xl">
-              Inversiones <br /> Recopa sac
+            {/* <h1 className="uppercase text-green-dark text-6xl font-bold md:leading-snug md:text-7xl">
+               Inversiones <br /> Recopa
             </h1>
-            <p className="mt-5 text-[#6C7F77]">
+            <span className=" text-green-light text-5xl font-bold md:leading-snug md:text-5xl">
+              Econstruyendo <br /> tu futuro
+            </span> */}
+            <span className="uppercase text-green-light text-6xl font-bold md:leading-snug md:text-4xl">
+               Sobre nosotros
+            </span>
+            <br />
+            <span className="uppercase text-green-dark text-5xl font-bold md:leading-snug md:text-6xl">
+              ¿Quíenes Somos?
+            </span>
+            <p className=" text-green-dark text-[19px] mt-5 w-[700px]">
               {" "}
-              Es una empresa dedicada al desarrollo de proyectos de{" "}
+              <span className="uppercase"><b>Inversiones Recopa SAC</b></span> es una empresa dedicada al desarrollo de proyectos de{" "}
               <b>viviendas eco-sostenibles</b> y comercialización de terrenos
               ubicados a <a>10 minutos antes de Huacho</a>, a la altura del{" "}
               <b>KM 136</b> de la <b>Panamericana Norte</b>, y a 10 minutos de
@@ -37,13 +47,13 @@ export function Overview() {
               </button>
             </div>
           </div>
-          <div className="flex justify-center bg-green-dark md:w-[700px] h-auto">
+          <div className="flex justify-center w-145 md:w-auto lg:w-auto">
             <Image
-              src="/assets/overview.jpg"
+              src="/assets/preventa.png"
               alt="Overview"
-              width={700}
-              height={200}
-              className="opacity-65"
+              width={1200}
+              height={400}
+              className="shadow-xl rounded-xl"
             />
           </div>
         </div>
